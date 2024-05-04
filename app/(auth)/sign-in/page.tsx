c3106d9b570bab7f16605cd3ffa68f69aed43ccb
Sadam Hussain <d_shussain@universalweather.net>
2024-05-04 01:05:30 -0400

    Setup Initial Pages

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type SignInPageProps = {};
+
+const SignInPage: React.FC<SignInPageProps> = () => {
+  return <>SignInPage</>;
+};
+
+export default SignInPage;
---
+++ app/(auth)/sign-out/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type SignOutPageProps = {};
+
+const SignOutPage: React.FC<SignOutPageProps> = () => {
+  return <>SignOutPage</>;
+};
+
+export default SignOutPage;
